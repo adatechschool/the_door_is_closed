9b201360ccb8ad012dd6e9b23e12d8aedf8ca0a0
Nydragon00 <6582485n@gmail.com>
2020-01-29 16:46:35 +0100

    updated multiple files
    bash - relative path functioning
    html - token functions properly and input can be passed to views.py for processing, thanks to Django Templates
    views.py - restructured funcitons into files

--- read_history/exec_sql.sh
+++ read_history/exec_sql.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-chrome_hist="C:\Documents and Settings\Nydragon\Local Settings\Application Data\Google\Chrome\User Data\Default\History"
 script_sql="script_get_history.sql"
-script_sql="C:\Users\Nydragon\Documents\GitHub\the_door_is_closed\read_history\script_get_history.sql"
 
-echo $chrome_hist
-echo $script_sql
+script_bsh=$(dirname $(realpath -s $0))
+script_sql="/script_get_history.sql"
+exec_tran="${script_bsh}${script_sql}"
 
-sqlite3 $chrome_hist < $script_sql
-# sqlite3 "..\script_get_history.sql"
-
+sqlite3 < $exec_tran
 read
